3c1fbeba49be46216d0846b63bb246e6f64aaa7f
Russell Johnson <rusljohn@gmail.com>
2019-11-14 10:39:35 +1300

    fixed up comments and made a version for home

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var brewries = [
 		id : 101,
 		name : 'Panhead',
 		latitude : -41.131073,
-		locations : 175.067505,
+		longditude : 175.067505,
 		openDays : ['Tue' , 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm']
 	},
@@ -13,7 +13,7 @@ var brewries = [
 		id : 102,
 		name : 'Boneface',
 		latitude : -41.130121, 
-		locations : 175.068525,
+		longditude : 175.068525,
 		openDays : ['Tue' , 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm', '11am - 9pm']
 	},
@@ -21,7 +21,7 @@ var brewries = [
 		id : 103,
 		name : 'Kereru',
 		latitude : -41.129686, 
-		locations : 175.072361,
+		longditude : 175.072361,
 		openDays : ['Tue' , 'Wed', 'Thu', 'Fri', 'Sat'],
 		openHours : ['9am - 4pm', '9am - 4pm', '9am - 4pm', '10am - 5pm', '10am - 5pm']
 	},
@@ -29,7 +29,7 @@ var brewries = [
 		id : 104,
 		name : 'Heyday Beer Co',
 		latitude : -41.295940, 
-		locations : 174.773553,
+		longditude : 174.773553,
 		openDays : ['Mon', 'Tue' , 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 11pm', '3pm - 11pm', '11am - 10pm', '11am - 10pm', '11am - 12am', '11am - 12am', '11am - 9pm']
 	},
@@ -37,7 +37,7 @@ var brewries = [
 		id : 105,
 		name : 'Garage Project Cellar Dorr',
 		latitude : -41.294491,  
-		locations : 174.767862,
+		longditude : 174.767862,
 		openDays : ['Mon', 'Tue' , 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['12pm - 7pm', '12pm - 8.30pm', '12pm - 8.30pm', '12pm - 8.30pm', '10am - 9.30pm', '10am - 9.30pm', '12pm - 7pm']
 	},
@@ -45,7 +45,7 @@ var brewries = [
 		id : 106,
 		name : 'Parrotdog Brewery',
 		latitude : -41.326756,   
-		locations : 174.799773,
+		longditude : 174.799773,
 		openDays : ['Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 7pm', '1pm - 7pm', '11am - 8pm', '11am - 8pm', '12pm - 6pm']
 	},
@@ -53,7 +53,7 @@ var brewries = [
 		id : 107,
 		name : 'Baylands Brewery',
 		latitude : -41.225268,   
-		locations : 174.873098,
+		longditude : 174.873098,
 		openDays : ['Tue' , 'Wed', 'Thu', 'Fri', 'Sat'],
 		openHours : ['9am - 5.30pm', '9am - 5.30pm', '9am - 5.30pm', '9am - 5.30pm', '11am - 5.30pm']
 	},
@@ -61,7 +61,7 @@ var brewries = [
 		id : 108,
 		name : 'Double Vision Brewing',
 		latitude : -41.309406,    
-		locations : 174.821354,
+		longditude : 174.821354,
 		openDays : ['Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 10.30pm', '3pm - 11.30pm', '12pm - 11.30pm', '12pm - 9.00pm'] 
 	},
@@ -69,7 +69,7 @@ var brewries = [
 		id : 109,
 		name : 'Whistling Sisters Beer Co',
 		latitude : -41.294065,     
-		locations : 174.777728,
+		longditude : 174.777728,
 		openDays : ['Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['3pm - 12am', '3pm - 12.00am', '3pm - 12am', '11:30am – 1am', '10am - 1am', '10am - 12am'] 
 	},
@@ -77,7 +77,7 @@ var brewries = [
 		id : 110,
 		name : 'Fork and Brewer',
 		latitude : -41.289235,     
-		locations : 174.775628,
+		longditude : 174.775628,
 		openDays : ['Mon', 'Tue' , 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
 		openHours : ['11.30am - 11pm', '11.30am - 12am', '11.30am - 12am', '11.30am - 12am', '11.30am - 10pm', '11.30am - 12am', '11.30am - 10pm'] 
 	}
@@ -107,6 +107,7 @@ $('#hideMap').on('click', function(){
 */
 
 // accessing apiKey from config.json
+// apiKey = '[{"key" : "yourKeyGoesHere"}]';
 var myKey = JSON.parse(apiKey); // Convert JSON data into js object
 console.log(myKey[0].key);
 
@@ -117,14 +118,15 @@ script.src = 'https://maps.googleapis.com/maps/api/js?key=' + myKey[0].key + '&c
 // Appending to the body of index.html
 document.getElementsByTagName('body')[0].appendChild(script); 
 
-
+var brewriesPosition = [{}];
 var breweryMap;
 function initMap() {
 	// Works out the location of the marker
 	var forkAndBrewer = {
-			lat: -41.289235, 
-			lng: 174.775628
-		};
+		lat: -41.289235, 
+		lng: 174.775628
+	};
+	
 	// Initialises the google map so that it displays on the web page
 	breweryMap = new google.maps.Map(document.getElementById('map'), {
 		center: {
